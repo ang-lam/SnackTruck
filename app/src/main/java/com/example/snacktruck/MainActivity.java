2eaab0c51c2e56c5cd331f9b16fe3f4bbb5bc57f
Angela <angelalamluu@gmail.com>
2021-12-22 15:21:09 -0800

    onclicklistener displays toast for submit button

--- app/src/main/java/com/example/snacktruck/MainActivity.java
+++ app/src/main/java/com/example/snacktruck/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.snacktruck;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button submit_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        submit_button = findViewById(R.id.btn_submit);
+
+        //click listener for button
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "You submitted your order!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
